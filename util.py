59c58ce26707f3a273a5b6f881cff2093d533e67
deepakrana47 <deepaksinghrana049@gmail.com>
2018-09-10 07:35:01 +0530

    RNN implemention tensor flow complete

--- util.py
+++ util.py
@@ -21,3 +21,20 @@ def all_parity_pairs(nbit):
                 X[ii, j] = 1
         Y[ii] = X[ii].sum() % 2
     return  X, Y
+
+def all_parity_pairs_with_sequence_labels(nbit):
+    X, Y = all_parity_pairs(nbit)
+    N, t = X.shape
+
+    # we want every time step to have a label
+    Y_t = np.zeros(X.shape, dtype=np.int32)
+    for n in range(N):
+        ones_count = 0
+        for i in range(t):
+            if X[n,i] == 1:
+                ones_count += 1
+            if ones_count % 2 == 1:
+                Y_t[n,i] = 1
+
+    X = X.reshape(N, t, 1).astype(np.float32)
+    return X, Y_t
